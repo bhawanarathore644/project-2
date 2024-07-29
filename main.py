0bb28ab4193d0e38ab4d83a329eca286e2a1f98a
nayakbhawana644 <nayakbhawana650@gmail.com>
2024-07-29 14:01:50 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+n = random.randint(1,100)
+a = -1
+guesses = 1
+while(a != n):
+    a = int(input("Guess the number:"))
+    if(a>n):
+        print("lower number please")
+        guesses += 1  
+        
+    elif(a<n):
+        print("Higher number please")
+        guesses += 1  
+
+print(f"You have guessed the number {n} correctly in {guesses} attempts")
